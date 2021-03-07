7ae503bea9b1a58d450cd55f0c650c9241c1c7d8
surbhi <surbhi.singh.1295@gmail.com>
2021-03-07 13:48:53 +0530

    first commit

---
+++ src/test/java/com/webservices/RestFullWebservies/RestFullWebserviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservices.RestFullWebservies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFullWebserviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
