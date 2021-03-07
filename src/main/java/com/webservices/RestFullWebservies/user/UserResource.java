c5d9bf7494d2a037b6e72594fa9b6e8246f56d58
surbhi <surbhi.singh.1295@gmail.com>
2021-03-07 17:02:20 +0530

    Delete Http implementation

--- src/main/java/com/webservices/RestFullWebservies/user/UserResource.java
+++ src/main/java/com/webservices/RestFullWebservies/user/UserResource.java
@@ -6,11 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
@@ -46,6 +42,14 @@ public class UserResource {
 			.buildAndExpand(savedUser.getId()).toUri();
 		
 		return ResponseEntity.created(location).build();
+		
+	}
+
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id){
+		User user = service.deleteById(id);
+		if(user==null)
+			throw new UserNotFoundException("id - "+id);
 		
 	}
 }
