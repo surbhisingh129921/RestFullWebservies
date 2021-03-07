f5745528f63dcc2594634ccaed5f464aaf22bb2e
surbhi <surbhi.singh.1295@gmail.com>
2021-03-07 14:44:10 +0530

    Added UserNotFoundException in Generic Exception Handler

--- src/main/java/com/webservices/RestFullWebservies/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/webservices/RestFullWebservies/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.webservices.RestFullWebservies.exception;
 
+import com.webservices.RestFullWebservies.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,6 +20,12 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
     ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundExceptions(UserNotFoundException ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 
 }
